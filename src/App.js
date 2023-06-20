550f3e705fee34d412d4c587ac235e2c8c7ef8e8
Gayathri2113 <gayathribhukya211@gmail.com>
2023-06-20 15:23:15 +0530

    Created a layout for weather-app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="box">
+        <form>
+          <input type="search" placeholder="Enter a city"></input>
+          <button className="btn1">Submit</button>
+          <button className="btn2">Current</button>
+        </form>
+        <h1>Paris</h1>
+        <h3>Tuesday 15:00 minutes</h3>
+        <h3>Clouds</h3>
+        <div className="weather">
+          <img />
+          <h1>20°C</h1>
+          <ul>
+            <li>Precipitation:</li>
+            <li>Wind:</li>
+          </ul>
+        </div>
+        <div className="forecast">
+          <div className="weather-forecast">
+            <p>Wed</p>
+            <img />
+            <p>13°</p>
+          </div>
+          <div className="weather-forecast">
+            <p>Thu</p>
+            <img />
+            <p>13°</p>
+          </div>
+          <div className="weather-forecast">
+            <p>Fri</p>
+            <img />
+            <p>13°</p>
+          </div>
+          <div className="weather-forecast">
+            <p>Sat</p>
+            <img />
+            <p>13°</p>
+          </div>
+          <div className="weather-forecast">
+            <p>Sun</p>
+            <img />
+            <p>13°</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
